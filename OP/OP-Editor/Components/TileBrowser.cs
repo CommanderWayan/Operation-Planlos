5ec6d5d50cd8347f932ca84b18049d43c8db4c1c
Tsett <obi_van04@yahoo.de>
2011-10-21 00:33:32 +0200

    mla was raus geschmissen

--- OP/OP-Editor/Components/TileBrowser.cs
+++ OP/OP-Editor/Components/TileBrowser.cs
@@ -125,13 +125,8 @@ namespace OP_Editor.Components
             if (_textureSheet == null) 
                 return;
 
-			//TODO: Flackerfrei zeichnen UND! hinkriegen das der Rahmen weg geht!
             Graphics gfx = CreateGraphics();
-			Graphics tempGfx = CreateGraphics();
 			gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
-			tempGfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
-
-			tempGfx.Clear(Color.Transparent);
             for (int x = 0; x < _textureSheet.XTiles; x++)
             {
                 for (int y = 0; y < _textureSheet.YTiles; y++)
@@ -140,17 +135,9 @@ namespace OP_Editor.Components
 													new Rectangle(x * _textureSheet.TileWidth + vScrollBar.Width - hScrollBar.Value * _textureSheet.TileWidth,
 																	y * _textureSheet.TileHeight + hScrollBar.Height - vScrollBar.Value * _textureSheet.TileHeight,
 																	_textureSheet.TileWidth,
-																	_textureSheet.TileHeight);
-					tempGfx.DrawImageUnscaledAndClipped(_textureSheet[x, y],cutRec);
-
-					Rectangle drawRec = new Rectangle(x * _textureSheet.TileWidth + vScrollBar.Width,
-																	y * _textureSheet.TileHeight + hScrollBar.Height,
-																	_textureSheet.TileWidth-1,
-																	_textureSheet.TileHeight-1);
-					if (_selectedTile == new Point(x, y))
-					{
-						tempGfx.DrawRectangle(new Pen(Color.Red), drawRec);
-					}
+																	_textureSheet.TileHeight)
+													);
+					
                 }
             }
 			
