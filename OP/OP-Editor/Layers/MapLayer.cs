4c6195f559462b830d2a4929bb987f430262c366
CommanderWayan <wayan-mail@gmx.net>
2011-10-25 16:12:47 +0200

    bissl map/layer gespiele

---
+++ OP/OP-Editor/Layers/MapLayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace OP_Editor.Layers
+{
+    class MapLayer
+    {
+        Point[,] _tiles;
+        float _parallaxFactor;
+
+        public MapLayer(int Width, int Height)
+        {
+            this._tiles = new Point[Width, Height];
+        }
+        public void setTile(int x, int y)
+        {
+        }
+    }
+}
